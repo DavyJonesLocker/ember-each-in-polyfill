7f54b1a6a0fdefb5758d11dedf7fc7b68ce62f61
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2016-02-19 12:14:51 -0500

    Handle non-objects gracefully

--- tests/integration/components/each-in-test.js
+++ tests/integration/components/each-in-test.js
@@ -15,7 +15,7 @@ test('It does what an each-in does', function(assert) {
     {{#each-in object as |key value|}}{{key}}:{{value}}{{/each-in}}
   `);
 
-  assert.equal(this.$().text().trim(), 'foo:barbaz:qux');
+  assert.equal(this.$().text().trim(), 'foo:barbaz:qux', 'should render key-value pairs');
 });
 
 test('It can yield to inverse like an each-in should', function(assert) {
@@ -25,5 +25,16 @@ test('It can yield to inverse like an each-in should', function(assert) {
     {{#each-in object as |key value|}}BAD{{else}}GOOD{{/each-in}}
   `);
 
-  assert.equal(this.$().text().trim(), 'GOOD');
+  assert.equal(this.$().text().trim(), 'GOOD', 'should only render inverse block');
+});
+
+test('It can handle non-objects gracefully', function(assert) {
+  this.render(hbs`
+    {{~#each-in "lol" as |key value|~}}BAD{{else}}1{{~/each-in~}}
+    {{~#each-in 123 as |key value|~}}BAD{{else}}2{{~/each-in~}}
+    {{~#each-in null as |key value|~}}BAD{{else}}3{{~/each-in~}}
+    {{~#each-in undefined as |key value|~}}BAD{{else}}4{{~/each-in~}}
+  `);
+
+  assert.equal(this.$().text().replace(/\W+/g, ''), '1234', 'should only render inverse blocks');
 });
